27557f58d8206191ba03031269d33adef1a0d770
Ngoc Phuong <nguyenngocphuongnb@gmail.com>
2018-03-21 21:24:17 +0700

    Add subapp support

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 'use strict';
 
 process.env.APP_ROOT = __dirname;
+process.env.LOG_WARN_PATH = process.env.APP_ROOT + '/private/logs/laco_warn.log';
+process.env.LOG_ERROR_PATH = process.env.APP_ROOT + '/private/logs/laco_error.log';
+process.env.LOG_COMBINED_PATH = process.env.APP_ROOT + '/private/logs/laco.log';
 const Laco = require('laco');
 
-new Laco().start();
+let laco = new Laco.Laco();
+let blog = new Laco.SubApp();
+
+blog.router.register({
+    path: '/',
+    name: 'home',
+    label: 'Home',
+    get: (req, res, next) => res.end('Home')
+});
+
+laco.subapp.register('/', blog);
+
+laco.start();
+laco.on('started', app => {
+    app.logger.info('Ahihi it\'s started');
+});
