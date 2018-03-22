0936d0acef4fe333d82944325c0fa409b3dbf98a
Phuong Nguyen <nguyenngocphuongnb@gmail.com>
2018-03-22 18:07:22 +0700

    Refactor router and subapp

--- index.js
+++ index.js
@@ -7,18 +7,7 @@ process.env.LOG_COMBINED_PATH = process.env.APP_ROOT + '/private/logs/laco.log';
 const Laco = require('laco');
 
 let laco = new Laco.Laco();
-let blog = new Laco.SubApp();
-
-blog.router.register({
-    path: '/',
-    name: 'home',
-    label: 'Home',
-    get: (req, res, next) => res.end('Home')
-});
-
-laco.subapp.register('/', blog);
-
-laco.start();
+laco.on('ready', () => laco.start());
 laco.on('started', app => {
-    app.logger.info('Ahihi it\'s started');
+    app.logger.info('[main] App started!');
 });
